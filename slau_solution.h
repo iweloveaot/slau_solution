a552900521324dcbe5ab9b208e60a5445d0002b2
vinny <viknikgon@yandex.ru>
2026-03-23 04:00:20 +0300

    исправлены ошибки + отчёт

--- slau_solution.h
+++ slau_solution.h
@@ -1,14 +1,13 @@
 #ifndef _SLAY_SOLUTION_H_
 #define _SLAY_SOLUTION_H_
 
-void generateMatrix(double**, int);
-void generateAnswers(double*, int);
+
 void LU(double**, double**, double**, int);
 void LUsolve(double**, double**, double*, double*, int);
 void Qmatrix(double**, double**, int);
 void Rmatrix(double**, double**, double**, int);
 void QRsolve(double**, double**, double*, double*, int);
 
-long long LUtime(double**, double**, double**, int);
+
 
 #endif /*_SLAY_SOLUTION_H_*/
